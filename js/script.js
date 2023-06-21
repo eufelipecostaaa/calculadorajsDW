07b8bf6478fdb4c2128446da7a89d20369f30a7a
eufelipecostaaa <feelingswp9@gmail.com>
2023-06-21 12:53:08 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function adicionar_parametro(parametro){
+    document.querySelector("[name='tela']").value += parametro
+}
+
+function calcular() {
+    conta = document.querySelector("[name='tela']").value
+    document.querySelector("[name='tela']").value = eval(conta)
+}
+
+function porcentagem() {
+    conta = document.querySelector("[name='tela']").value + '/ 100'
+    document.querySelector("[name='tela']").value = eval(conta)
+}
+
+function botaoAC() {
+    document.querySelector("[name='tela']").value = ''
+}
